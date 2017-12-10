e9be2f49ba517e736e7c216189b09b100ad203ff
Anani Oluseyi <anasey@outlook.com>
2017-12-10 15:21:11 -0500

    GET Method in the CRUD app

--- routes/note_routes.js
+++ routes/note_routes.js
@@ -1,4 +1,20 @@
+const ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db){
+        app.get('/notes:id', (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const details = {'_id' : new ObjectID(id)}
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err){
+                res.send({'error' : 'An error has occured'});
+            }else{
+                res.send(item);
+            }
+        });
+    });
+
+
     app.post('/notes', (req, res) => {
         const note = { text: req.body.body, title: req.body.title}
         db.collection('notes').insert(note, (err, result) => {
@@ -7,6 +23,6 @@ module.exports = function(app, db){
             }else{
                 res.send(result.ops[0]);
             }
-        })
+        });
     });
 }
